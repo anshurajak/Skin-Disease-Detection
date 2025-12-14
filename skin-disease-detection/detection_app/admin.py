32021b720efed0d392f5949e499224309bdd25b5
Anshu Kumar Rajak <149255817+anshurajak@users.noreply.github.com>
2025-12-14 09:37:08 +0530

    Delete skin-disease-detection directory

--- skin-disease-detection/detection_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import SkinDisease, SkinDiseasePrediction ,Feedback
-
-admin.site.register(SkinDisease)
-admin.site.register(SkinDiseasePrediction)
-admin.site.register(Feedback)
--- skin-disease-detection/skin_disease_detection/asgi.py
+++
@@ -1,5 +0,0 @@
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skin_disease_detection.settings')
-
-application = get_asgi_application()
